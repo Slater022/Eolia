e43704b5b464895d96b0e0ee1bd5be8a6f334d07
Erik <liquidgithub@hotmail.com>
2013-04-13 17:54:56 +0200

    Changed some stuff and added basic things.
    Every tile entity is based on BasicMachineTileEntity

---
+++ src/minecraft/eolia/tileentity/WindMillTileEntity.java
@@ -0,0 +1,8 @@
+package eolia.tileentity;
+
+import universalelectricity.prefab.network.IPacketReceiver;
+
+public class WindMillTileEntity extends BasicMachineTileEntity implements IPacketReceiver
+{
+	
+}
